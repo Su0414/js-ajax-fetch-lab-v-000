48f2068a914a1b65750a231e167dcbf7d0990379
Suchitra Naik <susanaik@gmail.com>
2018-08-30 18:05:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,10 +13,17 @@ function showResults(json) {
 function forkRepo() {
   const repo = 'learn-co-curriculum/javascript-fetch-lab'
   //use fetch to fork it!
+  const token = getToken()
+     
+    fetch('https://api.github.com/user/repos', {
+      headers: {
+        Authorization: `token ${token}`
+      }
+    }).then(res => res.json()).then(json => console.log(json));
 }
 
 function getToken() {
   //change to your token to run in browser, but set
   //back to '' before committing so all tests pass
-  return ''
+  return '9ef7770620101713a8660b504e95b8ec47ec64e7'
 }
